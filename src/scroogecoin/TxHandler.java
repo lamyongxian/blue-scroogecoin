21313400ff16859b02470b7c875ad9d8f7dcfcc6
unknown <yongxian@lam.sg>
2017-07-30 13:52:26 +0800

    Completed TxHandler

--- src/scroogecoin/TxHandler.java
+++ src/scroogecoin/TxHandler.java
@@ -1,4 +1,25 @@
+/**
+ * Copyright (c) 2017 Lam YongXian. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *  
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *  
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package scroogecoin;
+
+import java.util.ArrayList;
+
 public class TxHandler {
 	
 	private UTXOPool utxoPool;
@@ -80,9 +101,42 @@ public class TxHandler {
      * transaction for correctness, returning a mutually valid array of accepted transactions, and
      * updating the current UTXO pool as appropriate.
      */
+    
     public Transaction[] handleTxs(Transaction[] possibleTxs) {
-        // IMPLEMENT THIS
-    	return null;
+        
+    	ArrayList<Transaction> validTxs = new ArrayList<Transaction>();
+    	
+    	for (Transaction tx : possibleTxs) {
+    		if(this.isValidTx(tx)) {
+    			validTxs.add(tx);
+    			
+    			int currIndex = 0;
+    			
+    			for (Transaction.Output out : tx.getOutputs()) {
+    				UTXO currOutUTXO = new UTXO(tx.getHash(),currIndex);
+    				
+    				//add unspent utxo into utxo pool
+    				this.utxoPool.addUTXO(currOutUTXO, out);
+    				
+    				currIndex++;
+    			}
+    			
+    			for (Transaction.Input in : tx.getInputs()) {
+    				
+    	    		// Get UTXO for current transaction
+    	    		byte[] prevHash = in.prevTxHash;
+    	    		int oIndex = in.outputIndex;
+    	    		UTXO currInUTXO = new UTXO(prevHash,oIndex);
+    	    		
+    	    		//remove spent UTXO from UTXO pool
+    	    		this.utxoPool.removeUTXO(currInUTXO);
+    				
+    			}
+    		}
+    	}
+    	
+    	Transaction[] resultTxs = new Transaction[validTxs.size()];
+    	return validTxs.toArray(resultTxs);
     }
 
 }
